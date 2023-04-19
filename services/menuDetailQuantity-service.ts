d0e35aa71a2df15f2e09806c71b8ce4cdee2e4be
danielrios.exe <danielrios.exe@hotmail.com>
2023-04-18 21:01:09 -0500

    feat: added default structure to project and general server setup

---
+++ services/menuDetailQuantity-service.ts
@@ -0,0 +1,14 @@
+class ProductsService {
+  constructor() {}
+
+  /**
+   * TODO: This method was created in order to
+   * have a valid example of project structure.
+   * Add service-related code in a different branch
+   */
+  get() {
+    console.log('Hello world');
+  }
+}
+
+export default ProductsService;
