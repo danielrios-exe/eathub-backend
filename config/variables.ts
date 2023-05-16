24f6a72d9548a6976be3968d7882a35ba0485e72
Daniel Rios <danielrios.exe@hotmail.com>
2023-05-15 21:34:55 -0600

    chore: removed sqlserver and added postgresql dependencies and config

--- config/variables.ts
+++ config/variables.ts
@@ -8,10 +8,7 @@ const Variables = {
   environment: process.env.ENVIRONMENT || 'development',
   port: process.env.PORT,
   db: {
-    server: process.env.DB_SERVER,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    nonPooling: process.env.POSTGRES_URL_NON_POOLING,
   },
 };
 
