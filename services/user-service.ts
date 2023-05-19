7e2127b10590a1feb62e125a5698abb2910b1bb9
Daniel Rios <danielrios.exe@hotmail.com>
2023-05-18 22:07:26 -0600

    feat: added methods to validate if users exists if the credentials match and a method to create the user

--- services/user-service.ts
+++ services/user-service.ts
@@ -1,17 +1,71 @@
-import Database from '../db/Database';
-import { Request } from 'tedious';
+import pool from '../db/Database';
+import Errors from '../errors/errors';
+import AuthenticationService from './authentication-service';
+
+interface User {
+  username: string;
+  password: string;
+  name: string;
+  lastName: string;
+  email: string;
+  isActive: boolean;
+  roleId: number;
+}
 
 class UserService {
-  constructor() {}
+  /**
+   * Checks if a user exists in the database
+   * Used to create users
+   * @param username
+   * @param password
+   * @returns boolean
+   */
+  async isUserInDB(username: string): Promise<boolean> {
+    const request = await pool.query(
+      `select * from "user" u where u.username = '${username}'`
+    );
 
-  // Connection to database
-  databaseInstance = Database.getInstance();
+    return request.rowCount !== 0;
+  }
 
   /**
-   * TODO: Check if user is valid
+   * Checks if a user is in the database
+   * Used to authenticate users
+   * @param username
+   * @param password
+   * @returns boolean
    */
-  isUserValid(username: string, password: string): boolean {
-    return true;
+  async isUserValid(username: string, password: string): Promise<boolean> {
+    const authService = new AuthenticationService();
+    const request = await pool.query(
+      `select * from "user" u where u.username = '${username}'`
+    );
+
+    if (request.rowCount === 0) {
+      throw Errors.NO_USER_FOUND;
+    }
+
+    const user = request.rows[0];
+    const decryptedPassword = authService.decryptor(user.password);
+
+    return password === decryptedPassword;
+  }
+
+  async createUser(userObj: User) {
+    const authService = new AuthenticationService();
+    const request = await pool.query(
+      `INSERT INTO "user" ` +
+        `(username, password, name, last_name, email, is_active, role_id) ` +
+        `VALUES ('${userObj.username}', ` +
+        `'${authService.encryptor(userObj.password)}', ` +
+        `'${userObj.name}', '${userObj.lastName}', '${userObj.email}', ` +
+        `${userObj.isActive}, ${userObj.roleId})`
+    );
+    const rowCount = request.rowCount;
+
+    if (rowCount === 0) {
+      throw Errors.USER_NOT_CREATED;
+    }
   }
 }
 
